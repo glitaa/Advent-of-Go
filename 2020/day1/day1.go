90848647ab526fd3859f330ecee1267d4e4164f1
glitaa <adamek161@gmail.com>
2020-12-04 12:31:59 +0100

    Refine main function

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -51,21 +51,24 @@ func (y *Year2020) FindWithThreeNumbers(numbers []int) error {
 }
 
 func main() {
-	numbers := readNumbers()
 	myYear2020 := Year2020{}
 
-	/*myYear2020.FindWithTwoNumbers(numbers)
-	if err == nil {
-		fmt.Printf("Multiplication of two numbers that summed give 2020: %d \nThe three numbers are: %d, %d, %d\n", myYear2020.Multiplication, myYear2020.Numbers[0], myYear2020.Numbers[1], myYear2020.Numbers[2])
-	} else {
-		log.Fatalf("%s, given numbers: %v", err, numbers)
-	}*/
-
-	findErr := myYear2020.FindWithThreeNumbers(numbers)
+	fmt.Println("Enter numbers to look through to find 2020 with two of them:")
+	numbers := readNumbers()
+	findErr := myYear2020.FindWithTwoNumbers(numbers)
 	if findErr == nil {
+		fmt.Printf("Multiplication of two numbers that summed give 2020: %d \nThe two numbers are: %d, %d\n", myYear2020.Multiplication, myYear2020.Numbers[0], myYear2020.Numbers[1])
+	} else {
+		log.Fatalf("%s, given numbers: %v", findErr, numbers)
+	}
+
+	fmt.Println("Enter numbers to look through to find 2020 with three of them:")
+	numbers = readNumbers()
+	findErr2 := myYear2020.FindWithThreeNumbers(numbers)
+	if findErr2 == nil {
 		fmt.Printf("Multiplication of three numbers that summed give 2020: %d \nThe three numbers are: %d, %d, %d\n", myYear2020.Multiplication, myYear2020.Numbers[0], myYear2020.Numbers[1], myYear2020.Numbers[2])
 	} else {
-		log.Printf("%s, given numbers: %v", findErr, numbers)
+		log.Printf("%s, given numbers: %v", findErr2, numbers)
 	}
 }
 
