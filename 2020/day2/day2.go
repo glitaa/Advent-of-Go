fd45f57af39d35fd33b55a069310fd87c77502ba
glitaa <adamek161@gmail.com>
2020-12-05 22:55:59 +0100

    Add Password struct

---
+++ 2020/day2/day2.go
@@ -0,0 +1,14 @@
+package main
+
+import "strings"
+
+type Password struct {
+	Value                string
+	ContainsLetter       string
+	MinLetterOccurrences int
+	MaxLetterOccurrences int
+}
+
+func main() {
+
+}
