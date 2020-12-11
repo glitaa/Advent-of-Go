96ec499497ccf463a51b2b52821985693f7f6bc6
glitaa <adamek161@gmail.com>
2020-12-11 15:09:28 +0100

    Add readPasswordsFromFile function

--- 2020/day2/day2.go
+++ 2020/day2/day2.go
@@ -1,6 +1,13 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 type Password struct {
 	Value                string
@@ -18,5 +25,29 @@ func (p Password) IsValid() bool {
 }
 
 func main() {
+}
 
+func readPasswordsFromFile(path string) (passwords []Password) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	re := regexp.MustCompile("-|\\s|:\\s")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var password Password
+		password.MinLetterOccurrences, _ = strconv.Atoi(re.Split(scanner.Text(), -1)[0])
+		password.MaxLetterOccurrences, _ = strconv.Atoi(re.Split(scanner.Text(), -1)[1])
+		password.ContainsLetter = re.Split(scanner.Text(), -1)[2]
+		password.Value = re.Split(scanner.Text(), -1)[3]
+		passwords = append(passwords, password)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return passwords
 }
