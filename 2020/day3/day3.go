479a2a864280157d59e901ee983081e015d47173
Adam Glita <adamek161@gmail.com>
2021-01-01 19:24:35 +0100

    Add Check method

--- 2020/day3/day3.go
+++ 2020/day3/day3.go
@@ -2,5 +2,16 @@ package main
 
 type Slope [][]byte
 
+func (s Slope) Check() (trees int) {
+	x := 0
+	for y := 1; y < len(s); y++ {
+		x += 3
+		if s[y][x%(len(s[0])-1)] == '#' {
+			trees++
+		}
+	}
+	return
+}
+
 func main() {
 }
--- 2020/day3/day3_test.go
+++ 2020/day3/day3_test.go
@@ -1,1 +1,18 @@
 package main
+
+import "testing"
+
+func TestSlopeCheck(t *testing.T) {
+
+	input := "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
+
+	slope := MakeSlope(input)
+
+	got := slope.Check()
+	want := 7
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+
+}
